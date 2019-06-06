b1f27bdce768055f594f9edb17b405d3794a1f06
cp-mateusz <mateusz.jakowiec@computerplus.com.pl>
2019-06-06 15:24:19 +0200

    text stroke

--- src/main/java/com/jeyu/memegen/service/ImageProcessService.java
+++ src/main/java/com/jeyu/memegen/service/ImageProcessService.java
@@ -2,6 +2,9 @@ package com.jeyu.memegen.service;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -43,7 +46,8 @@ public class ImageProcessService {
             Size imageSize= getImageSize(image);
             Graphics2D graph = (Graphics2D) image.getGraphics();
 
-            overlayStringOnImage(graph, imageSize);
+            //overlayStringOnImage(graph, imageSize);
+            paintText(graph, imageSize);
             return encodeToString(image);
         }catch(final IOException ioe){
             throw new UncheckedIOException(ioe);
@@ -58,18 +62,75 @@ public class ImageProcessService {
         graph.setFont(graph.getFont().deriveFont(30f));
         graph.setStroke(new BasicStroke(5f));
 
-        List<String> textToOverlay = getTextToOverlay(this.textToOverlay, graph, size);
-        for (int i = 0; i < textToOverlay.size(); i++) {
+        FontMetrics fontMetrics = graph.getFontMetrics();
 
-            Size position = calculatePosition(textToOverlay.get(i), i, graph, size);
+        List<TextPosition> textToOverlay = getTextToOverlayWithPosition(this.textToOverlay, graph, size);
+        for (TextPosition textPosition : textToOverlay) {
 
-            graph.drawString(textToOverlay.get(i), position.getX(), position.getY());
+            Size position = textPosition.getPosition();
+
+            graph.drawString(textPosition.getText(), position.getX(), position.getY());
         }
 
         graph.dispose();
     }
 
 
+    private void paintText(Graphics2D graph, Size size){
+
+        graph.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        graph.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+
+        graph.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
+                RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+
+        graph.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
+                RenderingHints.VALUE_STROKE_NORMALIZE);
+
+        graph.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        Font impact = new Font("Impact", Font.BOLD, 30);
+        graph.setFont(impact);
+        graph.setFont(graph.getFont().deriveFont(30f));
+        graph.setStroke(new BasicStroke(0.5f));
+
+        FontRenderContext frc = graph.getFontRenderContext();
+
+
+        FontMetrics fontMetrics = graph.getFontMetrics();
+
+
+
+
+        AffineTransform transform;
+        AffineTransform oldForm = graph.getTransform();
+        List<TextPosition> textToOverlay = getTextToOverlayWithPosition(this.textToOverlay, graph, size);
+        for (TextPosition textPosition : textToOverlay) {
+            graph.setTransform(oldForm);
+            String s = textPosition.getText();
+            TextLayout textTl = new TextLayout(s, impact, frc);
+            Shape outline = textTl.getOutline(null);
+            transform = graph.getTransform();
+
+            Size position = textPosition.getPosition();
+
+
+            graph.setColor(Color.WHITE);
+            graph.drawString(textPosition.getText(), position.getX(), position.getY());
+            transform.translate(position.getX(), position.getY());
+            graph.transform(transform);
+            graph.setColor(Color.BLACK);
+            graph.draw(outline);
+
+
+
+        }
+
+    }
 
 
     String encodeToString(BufferedImage image){
@@ -100,6 +161,18 @@ public class ImageProcessService {
         return stringList;
     }
 
+    private List<TextPosition> getTextToOverlayWithPosition(String text, Graphics g, Size size){
+        List<String> textList = getTextToOverlay(text, g, size);
+        List<TextPosition> textPositions = new ArrayList<>();
+        for (int i = 0; i < textList.size(); i++) {
+            TextPosition textPosition = new TextPosition(textList.get(i),
+                    calculatePosition(textList.get(i), i, g.getFontMetrics(), size));
+            textPositions.add(textPosition);
+        }
+        return textPositions;
+
+    }
+
     String[] cutInHalf(String text){
         int middle = text.length() / 2;
         int before = text.lastIndexOf(" ", middle);
@@ -109,24 +182,24 @@ public class ImageProcessService {
         return str;
     }
 
-    private Size calculatePosition(String text, int index, Graphics g, Size size){
+    private Size calculatePosition(String text, int index, FontMetrics fontMetrics, Size size){
         Size position =new Size();
-        position.setX(getPosX(text, g, size));
-        position.setY(getPosY(index, g, size));
+        position.setX(getPosX(text, fontMetrics, size));
+        position.setY(getPosY(index, fontMetrics, size));
         return position;
     }
 
-    private int getPosY(int index, Graphics g, Size size) {
+    private int getPosY(int index, FontMetrics fontMetrics, Size size) {
         int posY = 0;
         switch (index){
             case 0:
                 posY = 30;
                 break;
             case 1:
-                posY = g.getFontMetrics().getHeight() + 40;
+                posY = fontMetrics.getHeight() + 40;
                 break;
             case 2:
-                posY = size.getY() - g.getFontMetrics().getHeight() - 20;
+                posY = size.getY() - fontMetrics.getHeight() - 20;
                 break;
             case 3:
                 posY = size.getY() - 10;
@@ -137,9 +210,9 @@ public class ImageProcessService {
         return posY;
     }
 
-    private int getPosX(String text, Graphics g, Size size) {
+    private int getPosX(String text, FontMetrics fontMetrics, Size size) {
         int halfWidth = size.getX() /2;
-        int halfStringWidth = g.getFontMetrics().stringWidth(text)/2;
+        int halfStringWidth = fontMetrics.stringWidth(text)/2;
         return halfWidth - halfStringWidth;
     }
 
@@ -171,5 +244,31 @@ public class ImageProcessService {
             this.y = y;
         }
 
+    }
+
+    class TextPosition{
+        private String text;
+        private Size position;
+
+        TextPosition(String text, Size position) {
+            this.text = text;
+            this.position = position;
+        }
+
+        String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        Size getPosition() {
+            return position;
+        }
+
+        public void setPosition(Size position) {
+            this.position = position;
+        }
     }
 }
